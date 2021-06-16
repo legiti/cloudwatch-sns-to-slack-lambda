430b2b8be0355116090bb5e8293cd3cc8ef2f3fb
Pearson Henri <pearsonhenri@gmail.com>
2021-06-16 09:32:15 -0700

    refactor: 💡 address comments

--- cloudwatch_sns_to_slack/constants.py
+++ cloudwatch_sns_to_slack/constants.py
@@ -9,17 +9,6 @@ HEADERS = {
     'Content-Type': 'application/json',
 }
 
-# # Slack channel IDs
-# PLATFORM_ALERTS_CHANNEL_ID = 'C0135LF58BH'
-# TEST_CHANNEL_ID = 'C01A6H83NKA'
-
 POST_TO_SLACK_WEBHOOK = 'https://hooks.slack.com/services/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'
 
 DEV_SNS_ARN = 'arn:aws:sns:sa-east-1:123456789012:test-topic'
-
-# Add a new channel via Slack here: https://api.slack.com/apps/A01VD8YBR28/incoming-webhooks
-# CHANNEL_TO_WEBHOOKS_MAP = {
-#     'platform-alerts': 'https://hooks.slack.com/services/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',
-#     'test_channel': 'https://hooks.slack.com/services/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',
-#     'area-51-aws': 'https://hooks.slack.com/services/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'
-# }
